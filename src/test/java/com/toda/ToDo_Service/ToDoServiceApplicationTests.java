b063086bda797d60a86af5935b82adb20b53fd3e
habiba <habibaahmedattia5@gmail.com>
2025-05-27 21:58:31 +0300

    setup project

---
+++ src/test/java/com/toda/ToDo_Service/ToDoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toda.ToDo_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
